80e065cdc48c3d836fbd34440a882d9512626dc0
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-22 15:01:59 +0530

    Add : here we are playing the game till the player reach to 100

--- src/com/bridgelabz/snakeandladderprogram/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladderprogram/SnakeAndLadder.java
@@ -5,25 +5,41 @@ import java.util.Random;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		// Here we are checking the position of the player
 
 		int playerPosition = 0;
+		int topPosition = 100;
 
 		Random rand = new Random();
+		 while (playerPosition < topPosition) {
+	            
+
 		int dice = rand.nextInt(6) + 1;
 
 		int option = rand.nextInt(3);
 		switch (option) {
 		case 0:
+			System.out.println("You got no Option");
 			break;
 		case 1:
+			System.out.println("You got Ladder");
 			playerPosition += dice;
+			int newPosition = playerPosition + dice;
+            if (newPosition <= topPosition) {
+                playerPosition = newPosition;
+            }
 			break;
 		case 2:
+			System.out.println("you got snake");
 			playerPosition -= dice;
+			  newPosition = playerPosition - dice;
+             if (newPosition >= 0) {
+                 playerPosition = newPosition;
+             }
+			
 			break;
 		}
 
-		System.out.println("Current position: " + playerPosition);
+		System.out.println("Player position: " + playerPosition);
+		 }
 	}
 }
