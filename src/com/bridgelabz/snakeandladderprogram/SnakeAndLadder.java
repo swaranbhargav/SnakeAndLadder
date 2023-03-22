f3882a90f20a3275cd1b897b5b304866afefa490
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-22 09:57:24 +0530

    Add : Here we are rolling the dice to get an output between 0 to 6

--- src/com/bridgelabz/snakeandladderprogram/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladderprogram/SnakeAndLadder.java
@@ -3,11 +3,14 @@ package com.bridgelabz.snakeandladderprogram;
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-		
-		       int playerPosition = 0;
+       //UC1- Here we are checking the position of the player
+        
+		int playerPosition = 0;
 
-		      
-		       System.out.println("Player Position:" + playerPosition);
+		System.out.println("Player Position:" + playerPosition);
+		//UC2- Here we are rolling the dice to get an output between 1 to 6.
+		int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
+		System.out.println("Roll Dice: " + dice);
 
 	}
 
