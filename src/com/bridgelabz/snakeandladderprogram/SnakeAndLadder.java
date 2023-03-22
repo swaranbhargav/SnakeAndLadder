e8fed470cf3ff858b7c4ade8abfa0e8a6c5e163e
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-22 08:59:11 +0530

    Add : Program to print the position of the player in snake and ladder game

---
+++ src/com/bridgelabz/snakeandladderprogram/SnakeAndLadder.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.snakeandladderprogram;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		
+		       int playerPosition = 0;
+
+		      
+		       System.out.println("Player Position:" + playerPosition);
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author SWARAN
+ *
+ */
+module SnakeAndLadder {
+}
