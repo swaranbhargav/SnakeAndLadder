d51b1808b6f2183536f14ed21db8d1ae64328695
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-22 15:20:11 +0530

    Add : we are checking how many times the dice rolled to reach 100

--- src/com/bridgelabz/snakeandladderprogram/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladderprogram/SnakeAndLadder.java
@@ -8,41 +8,44 @@ public class SnakeAndLadder {
 
 		int playerPosition = 0;
 		int topPosition = 100;
+		int diceroll = 0;
 
 		Random rand = new Random();
-		 while (playerPosition < topPosition) {
-	            
+		while (playerPosition < topPosition) {
 
-		int dice = rand.nextInt(6) + 1;
+			int dice = rand.nextInt(6) + 1;
 
-		int option = rand.nextInt(3);
-		switch (option) {
-		case 0:
-			System.out.println("You got no Option");
-			break;
-		case 1:
-			System.out.println("You got Ladder");
-			playerPosition += dice;
-			int newPosition = playerPosition + dice;
-            if (newPosition <= topPosition) {
-                playerPosition = newPosition;
-            }
-			break;
-		case 2:
-			System.out.println("you got snake");
-			playerPosition -= dice;
-			  newPosition = playerPosition - dice;
-             if (newPosition >= 0) {
-                 playerPosition = newPosition;
-             }else {
-            	 playerPosition = newPosition;
-             }
-			
-			break;
+			int option = rand.nextInt(3);
+			switch (option) {
+			case 0:
+				System.out.println("You got no Option");
+				break;
+			case 1:
+				System.out.println("You got Ladder");
+				playerPosition += dice;
+				int newPosition = playerPosition + dice;
+				if (newPosition <= topPosition) {
+					playerPosition = newPosition;
+				}
+				break;
+			case 2:
+				System.out.println("you got snake");
+				playerPosition -= dice;
+				newPosition = playerPosition - dice;
+				if (newPosition >= 0) {
+					playerPosition = newPosition;
+				} else {
+					playerPosition = newPosition;
+				}
+
+				break;
+				
+			}
+			diceroll++;
+			System.out.println("the no.of times the dice rolled is " + diceroll);
+
+			System.out.println("Player position: " + playerPosition);
 		}
-
-		System.out.println("Player position: " + playerPosition);
-		 }
-		 System.out.println("Congratulations! You reached the final position of " + topPosition);
+		System.out.println("Congratulations! You reached the final position of " + topPosition);
 	}
 }
