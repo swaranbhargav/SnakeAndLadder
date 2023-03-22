48597f0786ab993f3dc1bf01f1b9746ebcdc4e5d
“swaranbhargav” <swaranbhargav54@gmail.com>
2023-03-22 14:45:15 +0530

    Add : Here we are checking the position of the player

--- src/com/bridgelabz/snakeandladderprogram/SnakeAndLadder.java
+++ src/com/bridgelabz/snakeandladderprogram/SnakeAndLadder.java
@@ -1,17 +1,29 @@
 package com.bridgelabz.snakeandladderprogram;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
 	public static void main(String[] args) {
-       //UC1- Here we are checking the position of the player
-        
+		// Here we are checking the position of the player
+
 		int playerPosition = 0;
 
-		System.out.println("Player Position:" + playerPosition);
-		//UC2- Here we are rolling the dice to get an output between 1 to 6.
-		int dice = (int) Math.floor(Math.random() * 10) % 6 + 1;
-		System.out.println("Roll Dice: " + dice);
+		Random rand = new Random();
+		int dice = rand.nextInt(6) + 1;
 
+		int option = rand.nextInt(3);
+		switch (option) {
+		case 0:
+			break;
+		case 1:
+			playerPosition += dice;
+			break;
+		case 2:
+			playerPosition -= dice;
+			break;
+		}
+
+		System.out.println("Current position: " + playerPosition);
 	}
-
 }
